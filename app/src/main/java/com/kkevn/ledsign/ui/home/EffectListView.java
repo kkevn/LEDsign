398ec05eb0acae4339f7d07bc3532c27ea2d40c1
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2020-08-21 22:16:54 -0500

    updated listview with recyclerview

--- app/src/main/java/com/kkevn/ledsign/ui/home/EffectListView.java
+++ app/src/main/java/com/kkevn/ledsign/ui/home/EffectListView.java
@@ -1,17 +1,126 @@
 package com.kkevn.ledsign.ui.home;
 
-import android.app.Activity;
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.kkevn.ledsign.R;
 
 import java.util.Vector;
 
-public class EffectListView extends BaseAdapter {
+public class EffectListView extends RecyclerView.Adapter<EffectListView.ViewHolder> {
+
+    private Vector<Effect> effects;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    public EffectListView(Context context, Vector<Effect> effects) {
+        this.mInflater = LayoutInflater.from(context);
+        this.effects = effects;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.list_effects, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.iv_icon.setImageResource(getEffectIcon(position));
+        holder.tv_effect.setText(effects.get(position).getType());
+        holder.tv_param.setText(effects.get(position).getParam());
+        holder.iv_drag.setImageResource(R.drawable.baseline_drag_handle_24);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return effects.size();
+    }
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnCreateContextMenuListener {
+        ImageView iv_icon;
+        TextView tv_effect;
+        TextView tv_param;
+        ImageView iv_drag;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            iv_icon = itemView.findViewById(R.id.iv_icon);
+            tv_effect = itemView.findViewById(R.id.tv_effect);
+            tv_param = itemView.findViewById(R.id.tv_param);
+            iv_drag = itemView.findViewById(R.id.iv_drag);
+            itemView.setOnClickListener(this);
+            itemView.setOnCreateContextMenuListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null)
+                mClickListener.onItemClick(view, getAdapterPosition());
+        }
+
+
+        //@Override
+        public boolean onLongClick(View view) {
+            if (mClickListener != null)
+                mClickListener.onItemClick(view, getAdapterPosition());
+            return true;
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
+            contextMenu.setHeaderTitle("Effect Options:");
+            contextMenu.add(this.getAdapterPosition(), R.id.edit, 0, R.string.menu_edit);
+            contextMenu.add(this.getAdapterPosition(), R.id.remove, 1, R.string.menu_remove);
+        }
+    }
+
+    // convenience method for getting data at click position
+    Effect getItem(int id) {
+        return effects.get(id);
+    }
+
+    void removeItem(int i) {
+        //effects.remove(i);
+        effects.removeElementAt(i);
+        notifyItemRemoved(i);
+    }
+
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+        //boolean onItemLongClicked(int position);
+    }
+
+    private int getEffectIcon(int i) {
+
+        switch (effects.get(i).getType()) {
+            case Effect.EFFECT_TEXT_SCROLL:
+                return R.drawable.baseline_text_fields_24;
+
+            default:
+                return R.drawable.baseline_add_circle_outline_white_48dp;
+        }
+    }
+}
+
+/*public class EffectListView extends BaseAdapter {
     private Activity context;
 
     private Vector<Effect> effects;
@@ -81,4 +190,4 @@ public class EffectListView extends BaseAdapter {
                 return R.drawable.baseline_add_circle_outline_white_48dp;
         }
     }
-}
+}*/
--- app/src/main/java/com/kkevn/ledsign/ui/home/HomeFragment.java
+++ app/src/main/java/com/kkevn/ledsign/ui/home/HomeFragment.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -28,7 +31,7 @@ import java.util.Vector;
 import processing.android.PFragment;
 import processing.core.PApplet;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements EffectListView.ItemClickListener {
 
     private HomeViewModel homeViewModel;
 
@@ -36,10 +39,12 @@ public class HomeFragment extends Fragment {
 
     private static ListView lv_list;
 
+    static EffectListView adapter;
+
     private static Vector<Effect> effects_list = new Vector<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        //homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_new_profile, container, false);
 
         // setup Processing sketch
@@ -53,69 +58,54 @@ public class HomeFragment extends Fragment {
         fragment.setView(frame, getActivity());
 
         // setup custom effects list
-        lv_list = (ListView) root.findViewById(R.id.lv_list);
+        //lv_list = (ListView) root.findViewById(R.id.lv_list);
 
         //effects_list.clear();
         effects_list.add(new Effect(Effect.EFFECT_TEXT_SCROLL, "Hello there!"));
 
-        lv_list.setAdapter(new EffectListView(getContext(), effects_list));
+        //lv_list.setAdapter(new EffectListView(getContext(), effects_list));
 
-
-        // listener for item short press
-        onShortPress();
+        RecyclerView rv = root.findViewById(R.id.lv_list);
+        rv.setLayoutManager(new LinearLayoutManager(getContext()));
+        adapter = new EffectListView(getContext(), effects_list);
+        adapter.setClickListener(this);
+        rv.setAdapter(adapter);
+        //rv.animate();
 
         // register a context menu (which uses long press by default)
-        registerForContextMenu(lv_list);
-
+        registerForContextMenu(rv);
 
         return root;
     }
 
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(getContext(), "You clicked " + adapter.getItem(position).getType() + " on row number " + position, Toast.LENGTH_SHORT).show();
+    }
+
     public static void addEffect(String n, String p) {
         effects_list.add(new Effect(n, p));
-        ((BaseAdapter) lv_list.getAdapter()).notifyDataSetChanged();
-    }
-
-    /* listens for short press on list item and opens edit dialog */
-    public void onShortPress() {
-        lv_list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getContext(), "Edit Item", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
-
-    /* create the context menu */
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-
-        MenuInflater inflater = getActivity().getMenuInflater();
-        inflater.inflate(R.menu.effect_menu, menu);
+        //((BaseAdapter) lv_list.getAdapter()).notifyDataSetChanged();
+        adapter.notifyItemInserted(effects_list.size() - 1);
     }
 
     /* process clicks on the context menu */
     @Override
     public boolean onContextItemSelected(MenuItem item) {
 
-        // get index and id of list view item
-        int selection_index = ((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).position;
-        long selection_id = ((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).id;
-
         // determine action based on selected context menu
         switch(item.getItemId()) {
 
             // edit selected effect
             case R.id.edit:
-
                 Toast.makeText(getContext(), "Edit Item", Toast.LENGTH_SHORT).show();
                 return true;
 
             // remove selected effect from list
             case R.id.remove:
 
-                effects_list.remove(selection_index);
-                ((BaseAdapter) lv_list.getAdapter()).notifyDataSetChanged();
+                // context menu created with id's at adapter pos
+                adapter.removeItem(item.getGroupId());
 
                 Toast.makeText(getContext(), "Removed Item", Toast.LENGTH_SHORT).show();
                 return true;
