0926d9c6f18f27e925f337e4eb0987f721c456be
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2020-09-13 22:21:10 -0500

    added base layout and fragment for text scrolling effect configuration

--- app/src/main/java/com/kkevn/ledsign/SelectEffectDialogFragment.java
+++ app/src/main/java/com/kkevn/ledsign/SelectEffectDialogFragment.java
@@ -6,22 +6,19 @@ import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 
-import com.kkevn.ledsign.ui.create.CreateFragment;
-
 public class SelectEffectDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        //builder.setMessage(R.string.dialog_bt_profile)
         builder.setTitle(R.string.dialog_select_effect)
                 .setAdapter(MainActivity.selv, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         // The 'which' argument contains the index position of the selected item
 
+                        // obtain the selected effect and navigate to its configurator fragment
                         final String effect_name = MainActivity.selv.getItem(which).getType();
-                        //CreateFragment.addEffect(effect_name, "test");
                         MainActivity.configureEffect(effect_name);
                     }
                 })
--- app/src/main/java/com/kkevn/ledsign/ui/configurators/ScrollingTextFragment.java
+++ app/src/main/java/com/kkevn/ledsign/ui/configurators/ScrollingTextFragment.java
@@ -8,12 +8,17 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.kkevn.ledsign.R;
+import com.kkevn.ledsign.ui.create.CreateFragment;
+import com.kkevn.ledsign.ui.create.Effect;
 
 public class ScrollingTextFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_configure_scrolling_text, container, false);
+
+
+        //CreateFragment.addEffect(Effect.TEXT_SCROLL, "test");
 
         return root;
     }
