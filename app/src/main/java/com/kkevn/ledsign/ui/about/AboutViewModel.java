9df7a2f155ac93d361314f4c2e98762756895053
Kevin Kowalski <43073295+kkowal28@users.noreply.github.com>
2021-02-18 21:40:09 -0600

    added base files for about and help page fragments

---
+++ app/src/main/java/com/kkevn/ledsign/ui/about/AboutViewModel.java
@@ -0,0 +1,19 @@
+package com.kkevn.ledsign.ui.about;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.ViewModel;
+
+public class AboutViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public AboutViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is settings fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
