39da2329fdcfbd2c92dff3cd9add7b56f817c9db
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-03-08 00:34:49 -0600

    added app info and used library info to about page

--- app/src/main/java/com/kkevn/ledsign/ui/about/AboutFragment.java
+++ app/src/main/java/com/kkevn/ledsign/ui/about/AboutFragment.java
@@ -4,31 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
 
 import com.kkevn.ledsign.R;
 
 public class AboutFragment extends Fragment {
 
-    private AboutViewModel slideshowViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                ViewModelProviders.of(this).get(AboutViewModel.class);
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         View root = inflater.inflate(R.layout.fragment_about, container, false);
-        final TextView textView = root.findViewById(R.id.text_slideshow);
-        slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
         return root;
     }
 }
