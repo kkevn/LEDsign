3f9b2afabafb8a06c1794ae0a849759334886617
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-03-12 00:11:57 -0600

    added placeholder effects and fetching an effect's matrices as words

--- app/src/main/java/com/kkevn/ledsign/ui/create/Effect.java
+++ app/src/main/java/com/kkevn/ledsign/ui/create/Effect.java
@@ -7,9 +7,15 @@ public class Effect {
     public final static transient String TEXT_SCROLL = "Scrolling Text";
     public final static transient String COLOR_SOLID = "Solid Color";
     public final static transient String COLOR_RAINBOW = "Rainbow Wave";
+    public final static transient String COLOR_FADE = "Fade";
+    public final static transient String THEATER_CHASE = "Theater Chase";
+    public final static transient String MARQUEE = "Marquee";
+    public final static transient String RETRO = "Retro";
 
     public enum Effect_Types {
-        EFFECT_TEXT_SCROLL(TEXT_SCROLL), EFFECT_COLOR_SOLID(COLOR_SOLID), EFFECT_COLOR_RAINBOW(COLOR_RAINBOW);
+        EFFECT_TEXT_SCROLL(TEXT_SCROLL), EFFECT_COLOR_SOLID(COLOR_SOLID), EFFECT_COLOR_RAINBOW(COLOR_RAINBOW),
+        EFFECT_FADE(COLOR_FADE), EFFECT_THEATER_CHASE(THEATER_CHASE), EFFECT_MARQUEE(MARQUEE),
+        EFFECT_RETRO(RETRO);
 
         private final String stringValue;
 
@@ -45,5 +51,25 @@ public class Effect {
 
         // sort our readable params here maybe with switch?
         return this.param;
+    }
+
+    public String getMatrices(boolean asText) {
+
+        String[] matrices = {"Front", "Right", "Back", "Left", "Top"};
+
+        String sides = this.param.substring(1, this.param.indexOf(';'));
+
+        if (asText) {
+            String sidesAsText = "";
+
+            for (int i = 0; i < sides.length(); i++) {
+                if (sides.charAt(i) == '1') {
+                    sidesAsText += (matrices[i] + " - ");
+                }
+            }
+            return "[ " + sidesAsText.substring(0, sidesAsText.length() - 3) + " ]";
+        } else {
+            return sides;
+        }
     }
 }
