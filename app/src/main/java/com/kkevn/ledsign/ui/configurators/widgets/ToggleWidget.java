7df066b0b7d512a1e4effae2ea8649f0f5348ddc
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-03-19 00:43:57 -0500

    added a toggle widget for effect configurators

---
+++ app/src/main/java/com/kkevn/ledsign/ui/configurators/widgets/ToggleWidget.java
@@ -0,0 +1,63 @@
+package com.kkevn.ledsign.ui.configurators.widgets;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.kkevn.ledsign.R;
+import com.kkevn.ledsign.ui.create.Effect;
+
+public class ToggleWidget extends ConfiguratorWidget {
+
+    private View toggle;
+
+    private TextView tv_label;
+    private CheckBox cb_toggle;
+
+    public ToggleWidget(Context context, View root, String effect) {
+
+        // find matrix selection view in root layout
+        toggle = (View) root.findViewById(R.id.toggle);
+
+        // apply help dialog to help button
+        String help_message = context.getString(R.string.dialog_help_toggle);
+        toggle.findViewById(R.id.info_help).findViewById(R.id.ib_help).setOnClickListener(e -> cl.onHelpClick(help_message));
+
+        // find toggle label TextView and update its text for current effect
+        tv_label = toggle.findViewById(R.id.tv_toggle_label);
+        tv_label.setText("" + updateLabel(effect));
+
+        // find toggle CheckBox
+        cb_toggle = toggle.findViewById(R.id.cb_toggle);
+    }
+
+    private String updateLabel(String effect) {
+
+        // determine what attribute to use based on specified effect
+        switch (effect) {
+            case Effect.THEATER_CHASE:
+                return "Override color with rainbow";
+            default:
+                return "---";
+        }
+    }
+
+    @Override
+    public void updateWidgetInputs(String... inputs) {
+        // get toggle status from first and only input
+        int isToggled = Integer.parseInt(inputs[0]);
+
+        // set CheckBox based on toggle status
+        cb_toggle.setChecked(isToggled == 0 ? false : true);
+    }
+
+    @Override
+    public String parseWidgetInputs() {
+
+        // get the toggle status as an int
+        int isToggled = cb_toggle.isChecked() ? 1 : 0;
+
+        return "" + isToggled;
+    }
+}
