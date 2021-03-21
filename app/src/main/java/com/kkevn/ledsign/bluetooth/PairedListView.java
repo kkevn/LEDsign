4446e18d6eb39cfcddfc16ecdabdbc12c1876c9b
Kevin Kowalski <kowalski.d.kevin@gmail.com>
2021-03-21 18:48:03 -0500

    fixed Bluetooth paired devices list having incorrect text color based on current theme

--- app/src/main/java/com/kkevn/ledsign/bluetooth/PairedListView.java
+++ app/src/main/java/com/kkevn/ledsign/bluetooth/PairedListView.java
@@ -2,6 +2,9 @@ package com.kkevn.ledsign.bluetooth;
 
 import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.support.v7.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,9 +18,11 @@ public class PairedListView extends ArrayAdapter<BluetoothDevice> {
 
     private ArrayList<BluetoothDevice> mPairedDevices;
     private LayoutInflater mInflater;
+    private Context context;
 
     public PairedListView(Context context, ArrayList<BluetoothDevice> objects) {
         super(context, 0, objects);
+        this.context = context;
         this.mInflater = LayoutInflater.from(context);
         this.mPairedDevices = objects;
     }
@@ -51,9 +56,14 @@ public class PairedListView extends ArrayAdapter<BluetoothDevice> {
             viewHolder = (PairedListViewHolder) v.getTag();
         }
 
+        // get app theme to know which colors to apply to TextView items
+        String theme = PreferenceManager.getDefaultSharedPreferences(context).getString(context.getString(R.string.pref_theme_key), "");
+
         // populate item based on current position
         viewHolder.tv_device_name.setText(mPairedDevices.get(i).getName());
+        viewHolder.tv_device_name.setTextColor(theme.equals("0") ? Color.BLACK : Color.WHITE);
         viewHolder.tv_device_address.setText("[" + mPairedDevices.get(i).getAddress() + "]");
+        viewHolder.tv_device_address.setTextColor(theme.equals("0") ? Color.BLACK : Color.WHITE);
 
         // return the view
         return v;
